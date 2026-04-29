3b528624383000526672220257df8f666d382277
saketh252 <sakethvaransi4@gmail.com>
2026-04-29 06:28:01 +0530

    Time: 1 ms (85.85%), Space: 53.8 MB (93.82%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,34 +4,23 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let seen={}// creating a hasmap for numbers we have seen ie an object
-    // format number equals index 
+    let seen={}// store all the values in key values pairs
 
-    //looping through the array 
-    for(let i=0;i<nums.length; i++){
-        // creating an index at i
-        let current =nums[i];
+    for(let i=0;  i<nums.length; i++){
 
-        //calucuate the current number needed to reach the complement
-        let complement = target - current;
-        // check if the target already exist in seen if 'seen"
-        // if yes we have found the complement
+        let current =nums[i];// loop and break everything equally
 
-        if (seen[complement] !== undefined){
-            //return the indices
+        let complement =target-current;
+
+        if (seen[complement]!==undefined){
+
+
             return[seen[complement],i];
+
         }
-        // if not found,store the current nubers and its index
-           seen[current]=i
+        seen [current]=i;
     }
-}
+
 
         
-
-
-    
-
-
-    
-
-    
+};
