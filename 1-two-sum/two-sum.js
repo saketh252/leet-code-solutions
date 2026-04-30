71340aee4b95e4bb3fddf808a72b9f9b2a98d05b
saketh252 <sakethvaransi4@gmail.com>
2026-04-30 15:10:47 +0530

    Time: 0 ms (100.00%) | Memory: 53.7 MB (93.82%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -2,19 +2,30 @@
  //* @param {number[]} nums//
  //* @param {number} target//
  //* @return {number[]}
- var twoSum = function(nums, target) {
-    let map ={};//storing the hasmap in one place
+ var twoSum = function(nums, target){// function so that we reach target 
+    let map={};// storing the keys value pairs and also creating a hash map
+    
+    for (let i=0;i<nums.length; i++){
+        let complement = target - nums[i];
 
-    for (let i=0;i<nums.length;i++){//
-        let complement = target -nums[i];
+        if(map[complement]!==undefined){// if complement exists then we seen the nu
+    
+       return[map[complement],i];
+        } 
+       // return the indices the map complement
+       map[nums[i]]=i;//  store the current number index in the map 
 
-        if (map[complement]!== undefined){
-            return[map [complement],i];
+       
 
-        }
-        map[nums[i]]=i;
+        
     }
-};
+
+
+
+
+
+ };
+
 
 
         
